d86363ae7c5772868adbb14beb69e44b5ba3687c
maijatissarinen <maija.tissarinen@gmail.com>
2026-01-11 22:29:32 +0200

    Deployment works! Making nav logos visible

--- src/navigation/navbar.jsx
+++ src/navigation/navbar.jsx
@@ -13,8 +13,8 @@ function Navbar () {
                 <li><a href='#project'>{t("Nav.pro")}</a></li>
                 <li><a href='#work'>{t("Nav.work")}</a></li>
                 <li><a href='#edu'>{t("Nav.edu")}</a></li>
-                <li><a href='https://github.com/maijatissarinen'><img src='./public/github-mark.svg'></img></a></li>
-                <li><a href='https://linkedin.com/in/maijatissarinen'><img src='./public/InBug-Black.png'></img></a></li>
+                <li><a href='https://github.com/maijatissarinen'><img src='github-mark.svg'></img></a></li>
+                <li><a href='https://linkedin.com/in/maijatissarinen'><img src='InBug-Black.png'></img></a></li>
                 <li></li>
             </ul>
             <div className='desktop-lang'>
