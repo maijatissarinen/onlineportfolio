cf8259318ac0a2d2b410d7f30bcbce0553358114
maijatissarinen <maija.tissarinen@gmail.com>
2026-01-11 21:24:47 +0200

    Testing deployment

---
+++ my-app/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Navbar from './navigation/navbar.jsx';
+import {Routes, Route} from 'react-router-dom';
+import HomePage from './pages/homePage.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
